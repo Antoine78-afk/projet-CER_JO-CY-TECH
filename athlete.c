5436477295c114a5a7b49f6a901b08d7abc429fe
AntoineM <Antoinemarhic78@gmail.com>
2024-05-13 01:13:38 +0200

    update

---
+++ athlete.c
@@ -0,0 +1,22 @@
+typedef struct   {
+    char date [10];
+    char *epreuve;
+    float temps;
+
+
+
+
+} entrainement;
+
+
+
+typedef struct  {
+    
+    char *prenom;
+    float performance[]
+    
+
+
+    
+    
+};
