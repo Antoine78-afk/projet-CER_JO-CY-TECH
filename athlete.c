e13fd7f3205abd30c033905a07083deaa4dcfde2
AntoineM <Antoinemarhic78@gmail.com>
2024-05-14 19:02:15 +0200

    update

--- athlete.c
+++ athlete.c
@@ -1,22 +1,36 @@
+const int MAX_ENTRAINEMENTS = 10;
+#define MAX_athlete_relai 4
+
+
+
 typedef struct   {
-    char date [10];
-    char *epreuve;
+    char date [20];
+    char type_epreuve[20];
     float temps;
+    int position_relais;//pour le relai
 
 
 
 
-} entrainement;
+} Entrainement;
 
 
 
 typedef struct  {
-    
-    char *prenom;
-    float performance[]
-    
+    char nom[50];
+    float performance;
+    int nb_entrainement;
+    Entrainement entrainement[MAX_ENTRAINEMENTS];
 
 
     
     
-};
+};Athlete;
+
+
+
+typedef struct {
+    char date[20];
+    char type_epreuve[20];
+    float temps[MAX_athlete_relai]; // Temps de chaque participant
+} EntrainementRelais;
