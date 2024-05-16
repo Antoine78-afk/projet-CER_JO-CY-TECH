18a01e63f6c4440cd3255524ae60602299c05880
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-16 20:55:21 +0200

    Update structure.h

--- structure.h
+++ structure.h
@@ -1,1 +1,20 @@
 
+#define STRUCTURES_H
+
+#define MAX_ENTRAINEMENTS 100
+
+typedef struct {
+    char date[20];
+    char type_epreuve[20];
+    float temps;
+    int position_relais; // Pour le relais
+} Entrainement;
+
+typedef struct {
+    char nom[50];
+    int nb_entrainements;
+    Entrainement entrainements[MAX_ENTRAINEMENTS];
+} Athlete;
+
+#endif /* STRUCTURES_H */
+
