ea933038feb2f9cda1b0be8a466c82489a9a8249
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-16 20:56:01 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,29 +4,38 @@
 #include <assert.h>
 #include <stdbool.h>
 #include "affichage.c"
+#include "athlete.h"
 
-//#include "display.h"
-//#include "func.h"
-int main()
-{
-    menu();
+int main() {
+    Athlete athlete1;
+    strcpy(athlete1.nom, "John Doe");
+    athlete1.nb_entrainements = 0;
 
+    ajouterEntrainement(&athlete1, "2024-05-14", "100m", 10.5, -1);
+    ajouterEntrainement(&athlete1, "2024-05-15", "Relais", 0, 2);
 
+    consulterHistoriqueEntrainements(&athlete1);
+    consulterStatistiquesPerformance(&athlete1);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
