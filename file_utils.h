1683095554948f117e72c965710ec8a79528a766
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-20 18:14:34 +0200

    Update file_utils.h

--- file_utils.h
+++ file_utils.h
@@ -1,2 +1,10 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
 
+#include "performance.h"
+
+void sauvegarderPerformance(char* nom, Performance* performance);
+void lireHistorique(char* nom);
+
+#endif
 
