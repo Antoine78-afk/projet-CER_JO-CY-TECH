aac54e960df07ef1c8e2c89bcc8d2eeba1da6d38
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-21 00:09:42 +0200

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,7 +7,7 @@
 
 #ifndef STATS_H
 #define STATS_H
-
+// Déclaration de la fonction pour afficher les statistiques d'un athlète pour une épreuve donnée
 void afficherStats(char* nom, char* epreuve);
 
 #endif
