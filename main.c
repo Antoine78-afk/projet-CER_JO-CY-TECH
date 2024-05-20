4ccede200c9b8891c1eb1f2270e788b22d74644b
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-21 00:07:28 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,43 +5,40 @@
 #include "file_utils.h"
 #include "stats.h"
 
+// Cette fonction affiche le menu principal du programme
 void menu() {
-    printf("Gestion des performances des athlètes\n");
-    printf("1. Ajouter un nouvel entraînement\n");
-    printf("2. Consulter l'historique des entraînements\n");
-    printf("3. Consulter les statistiques\n");
-    printf("4. Quitter\n");
+    printf("Gestion des performances des athlètes\n"); // Titre du menu
+    printf("1. Ajouter un nouvel entraînement\n"); // Option pour ajouter un entraînement
+    printf("2. Consulter l'historique des entraînements\n"); // Option pour consulter l'historique
+    printf("3. Consulter les statistiques\n"); // Option pour consulter les statistiques
+    printf("4. Quitter\n"); // Option pour quitter le programme
 }
 
 int main() {
-    int choix;
+    int choix; // Variable pour stocker le choix de l'utilisateur
     do {
-        menu();
-        printf("Choisissez une option : ");
-        scanf("%d", &choix);
-        switch (choix) {
+        menu(); // Appelle la fonction pour afficher le menu
+        printf("Choisissez une option : "); // Demande à l'utilisateur de choisir une option
+        scanf("%d", &choix); // Lit le choix de l'utilisateur
+        switch (choix) { // Exécute une action en fonction du choix de l'utilisateur
             case 1:
-                ajouterEntrainement();
+                ajouterEntrainement(); // Appelle la fonction pour ajouter un entraînement
                 break;
             case 2:
-                consulterHistorique();
+                consulterHistorique(); // Appelle la fonction pour consulter l'historique des entraînements
                 break;
             case 3:
-                consulterStats();
+                consulterStats(); // Appelle la fonction pour consulter les statistiques
                 break;
             case 4:
-                printf("Au revoir!\n");
+                printf("Au revoir!\n"); // Message de sortie
                 break;
             default:
-                printf("Option invalide, veuillez réessayer.\n");
+                printf("Option invalide, veuillez réessayer.\n"); // Message pour un choix invalide
         }
-    } while (choix != 4);
+    } while (choix != 4); // Répète le menu jusqu'à ce que l'utilisateur choisisse de quitter
 
-    return 0;
+    return 0; // Retourne 0 pour indiquer que le programme s'est terminé correctement
 }
-
-
-
-
 
 
