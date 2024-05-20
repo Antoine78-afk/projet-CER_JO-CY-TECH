fc011eac4bb67159b1e46bfda9871e70856a1d94
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-21 00:07:49 +0200

    Update athlete.h

--- athlete.h
+++ athlete.h
@@ -5,13 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Définition de la structure Athlete
 typedef struct {
-    char nom[50];
-    char prenom[50];
-    char genre[10];
+    char nom[50]; // Nom de l'athlète
+    char prenom[50]; // Prénom de l'athlète
+    char genre[10]; // Genre de l'athlète
 } Athlete;
 
+// Déclaration de la fonction pour créer un athlète
 Athlete* creerAthlete(char* nom, char* prenom, char* genre);
+
+// Déclaration de la fonction pour afficher les informations d'un athlète
 void afficherAthlete(Athlete* athlete);
 
-#endif
+#endif // ATHLETE_H
