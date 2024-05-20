86223258c5b873e8246978a4f48b18bae8b153ef
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-20 18:11:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,62 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <assert.h>
-#include <stdbool.h>
 #include "athlete.h"
-#include "structure.h"
-#include "athlete.c"
-//void menu()
-//{
-    //printf("bienvenue dans l'application  préparation jo\n");  
-   // printf("pour choisir un athlète, entrer son numéro associé\n");
-    //printf("pour choisir une épreuve entrer sa lettre associé\n\n ");
+#include "performance.h"
+#include "file_utils.h"
+#include "stats.h"
 
-    //création des différentes épreuves et attribution d'une lettre à chaque épreuve
-    //char tab[5][20] = {"100m","200m","400m","marathon","relais 4*400"}; 
-       // printf("%c : %s \n",c,tab[c%97]);
-   // }
+void menu() {
+    printf("Gestion des performances des athlètes\n");
+    printf("1. Ajouter un nouvel entraînement\n");
+    printf("2. Consulter l'historique des entraînements\n");
+    printf("3. Consulter les statistiques\n");
+    printf("4. Quitter\n");
+}
 
-
-
-
-// pour que l'entraineur ajoute les entrainements    
-//void ajouterEntrainement(Athlete *athlete, char date[], char type_epreuve[], float temps, int position_relais) {
 int main() {
-    Athlete athlete1;
-    Athlete athlete2;
-    strcpy(athlete1.nom, "John Doe");
-    strcpy(athlete2.nom, "antoine marhic");
-    athlete1.nb_entrainements = 0;
-
-    ajouterEntrainement(&athlete1, "2024-05-14", "100m", 10.5, -1);
-    ajouterEntrainement(&athlete1, "2024-05-15", "Relais", 10, 2);
-    ajouterEntrainement(&athlete2, "2024-05-16", "100m", 12, -1);
-    ajouterEntrainement(&athlete2, "2024-05-16", "200m", 15, -1);
-
-
-
-    consulterHistoriqueEntrainements(&athlete1);
-    consulterStatistiquesPerformance(&athlete1);
-    consulterStatistiquesPerformance(&athlete2);
-    
+    int choix;
+    do {
+        menu();
+        printf("Choisissez une option : ");
+        scanf("%d", &choix);
+        switch (choix) {
+            case 1:
+                ajouterEntrainement();
+                break;
+            case 2:
+                consulterHistorique();
+                break;
+            case 3:
+                consulterStats();
+                break;
+            case 4:
+                printf("Au revoir!\n");
+                break;
+            default:
+                printf("Option invalide, veuillez réessayer.\n");
+        }
+    } while (choix != 4);
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
