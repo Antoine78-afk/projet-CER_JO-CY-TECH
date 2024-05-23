785366a67abb8c45188bea7ab02937fcf092391e
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-23 17:35:17 +0200

    Update athlete.c

--- athlete.c
+++ athlete.c
@@ -1,23 +1,16 @@
 #include "athlete.h"
 
-// Cette fonction crée une nouvelle instance d'un athlète avec les informations fournies
-Athlete* creerAthlete(char* nom, char* prenom, char* genre) {
-    // Allocation de mémoire pour une nouvelle structure Athlete
+// Crée un athlète et initialise ses informations
+Athlete* creerAthlete(char* nom, char* prenom) {
     Athlete* athlete = (Athlete*) malloc(sizeof(Athlete));
-    
-    // Copie des informations dans les champs de la structure Athlete
-    strcpy(athlete->nom, nom); // Copie le nom
-    strcpy(athlete->prenom, prenom); // Copie le prénom
-    strcpy(athlete->genre, genre); // Copie le genre
-    
-    // Retourne un pointeur vers la nouvelle structure Athlete
+    strcpy(athlete->nom, nom);
+    strcpy(athlete->prenom, prenom);
     return athlete;
 }
 
-// Cette fonction affiche les informations d'un athlète donné
+// Affiche les informations d'un athlète
 void afficherAthlete(Athlete* athlete) {
-    // Affiche le nom, le prénom et le genre de l'athlète
-    printf("Nom: %s, Prénom: %s, Genre: %s\n", athlete->nom, athlete->prenom, athlete->genre);
+    printf("Nom: %s, Prénom: %s\n", athlete->nom, athlete->prenom);
 }
 
 
