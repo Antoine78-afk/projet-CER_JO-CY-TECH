aac82dd1a27994f6a297fce14f32a5e4bb4c6d48
Antoine78-afk <84043697+Antoine78-afk@users.noreply.github.com>
2024-05-23 17:36:14 +0200

    Update file_utils.h

--- file_utils.h
+++ file_utils.h
@@ -1,12 +1,12 @@
+
 #ifndef FILE_UTILS_H
 #define FILE_UTILS_H
 
 #include "performance.h"
 
-// Déclaration de la fonction pour sauvegarder une performance dans un fichier
+// Sauvegarde une performance dans un fichier
 void sauvegarderPerformance(char* nom, Performance* performance);
-
-// Déclaration de la fonction pour lire l'historique des performances d'un athlète depuis un fichier
+// Lit l'historique des performances d'un athlète
 void lireHistorique(char* nom);
 
-#endif // FILE_UTILS_H
+#endif
